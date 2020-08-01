ed88e4a66b36bef4d721cce74941aa5be41090d9
unknown <thisisnew@naver.com>
2020-08-01 18:09:04 +0900

    Updates

---
+++ precache-manifest.6a547177346d6db5b964268ed44760d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54d5cf4f82f1e95ada17eca1b2482f5",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "84d20ffdce58279bc4af",
+    "url": "/movie_app_2020/static/css/main.81bcfa60.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d20ffdce58279bc4af",
+    "url": "/movie_app_2020/static/js/main.ca2aa889.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
